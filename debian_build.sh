b142c340dd1b277d4a5d1337095836bb4290b55e
oliver <48876432+gklkf@users.noreply.github.com>
2023-12-05 22:49:23 +0800

    Update for raphael, match Ubuntu 23.10 and remove useless code

--- debian_build.sh
+++ debian_build.sh
@@ -3,32 +3,23 @@
 set -eux
 
 setup_export() {
-    export KERNEL_PATH=$PWD
-    export CLANG_PATH=~/toolchains/neutron-clang
+    export KERNEL_PATH=~/Repos/kernels/kernel_xiaomi_cepheus
+    export ANYKERNEL_PATH=~/Repos/AnyKernel3
+    export BUILDER_PATH=$PWD
+    export CLANG_PATH=~/Repos/toolchains/proton-clang
     export PATH=${CLANG_PATH}/bin:${PATH}
-    export CLANG_TRIPLE=aarch64-linux-gnu-
+    # export CLANG_TRIPLE=aarch64-linux-gnu- # Enable when use google AOSP clang
     export ARCH=arm64
     export SUBARCH=arm64
-    export KERNEL_DEFCONFIG=vendor/lahaina-qgki_defconfig
-    export LLVM_VERSION=17
-    export SETUP_KERNELSU=true  # Enable if you want KernelSU
-    export KernelSU_TAG=main    # Select KernelSU tag or branch
-    # Custom Keystore hash and size for KernelSU Manager
-    # Use `ksud debug get-sign <apk_path>` to get them
-    if [ "${1-}" == "custom" ] || [ "${2-}" == "custom" ]; then
-        export KSU_EXPECTED_SIZE=0x352
-        export KSU_EXPECTED_HASH=f29d8d0129230b6d09edeec28c6b17ab13d842da73b0bc7552feb81090f9b09e
-    else
-        unset KSU_EXPECTED_SIZE
-        unset KSU_EXPECTED_HASH
-    fi
+    export KERNEL_DEFCONFIG=raphael_defconfig
+    export VOID_FIX_PATCH=false # Enable fix-void-err-patch;disable when use proton clang 13
+    export SETUP_ENVIRONMENT=false # Set up the build environment
+    export SETUP_KERNELSU=true # Enable if you want KernelSU
+    export USE_KPROBES=true # Integrate with kprobe or patch
+## Select KernelSU tag or branch; Stable tag: KERNELSU_TAG=stable; Dev branch: KERNELSU_TAG=main; Custom tag(Such as v0.5.2): KERNELSU_TAG=V0.5.2
+    export KERNELSU_TAG=stable  
+    # export ENABLE_LTO=false   # Enable LTO optimize
 }
-
-if [ "${1-}" == "clean" ] || [ "${2-}" == "clean" ]; then
-    test -d ~/.ccache && rm -rf ~/.ccache
-    test -d ~/.cache/ccache && rm -rf ~/.cache/ccache
-    test -d "$KERNEL_PATH/out" && rm -rf "$KERNEL_PATH/out"
-fi
 
 update_kernel() {
     cd $KERNEL_PATH
@@ -39,67 +30,76 @@ update_kernel() {
 setup_environment() {
     cd $KERNEL_PATH
     sudo apt update
-    sudo apt install zstd tar wget curl
+    sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib \
+                        git git-lfs gnupg gperf lib32readline-dev lib32z1-dev \
+                        libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils \
+                        lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
     if [ ! -d $CLANG_PATH ]; then
-      mkdir -p $CLANG_PATH
-      cd $CLANG_PATH
-      curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
-      bash antman -S=latest
-      bash antman --patch=glibc
+      git clone https://github.com/kdrag0n/proton-clang.git --depth=1 $CLANG_PATH
     fi
-    chmod +x llvm.sh
-    sudo ./llvm.sh $LLVM_VERSION
-    rm ./llvm.sh
-    sudo apt install --fix-missing
-    sudo ln -s --force /usr/bin/clang-$LLVM_VERSION /usr/bin/clang
-    sudo ln -s --force /usr/bin/ld.lld-$LLVM_VERSION /usr/bin/ld.lld
-    sudo ln -s --force /usr/bin/llvm-objdump-$LLVM_VERSION /usr/bin/llvm-objdump
-    sudo ln -s --force /usr/bin/llvm-ar-$LLVM_VERSION /usr/bin/llvm-ar
-    sudo ln -s --force /usr/bin/llvm-nm-$LLVM_VERSION /usr/bin/llvm-nm
-    sudo ln -s --force /usr/bin/llvm-strip-$LLVM_VERSION /usr/bin/llvm-strip
-    sudo ln -s --force /usr/bin/llvm-objcopy-$LLVM_VERSION /usr/bin/llvm-objcopy
-    sudo ln -s --force /usr/bin/llvm-readelf-$LLVM_VERSION /usr/bin/llvm-readelf
-    sudo ln -s --force /usr/bin/clang++-$LLVM_VERSION /usr/bin/clang++
+    if [ ! -d $ANYKERNEL_PATH ]; then
+       git clone -b raphael https://github.com/gklkf/AnyKernel3 --depth=1 $ANYKERNEL_PATH
+    fi
 }
 
 setup_kernelsu() {
     cd $KERNEL_PATH
-    curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-    # Enable KPROBES
-    scripts/config --file "arch/$ARCH/configs/$KERNEL_DEFCONFIG" -e MODULES -e KPROBES -e HAVE_KPROBES -e KPROBE_EVENTS
+    if test $KERNELSU_TAG = stable 
+    then
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - 
+    else
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s $KERNELSU_TAG
+    fi
+
+    if test $USE_KPROBES = true 
+    then
+        echo [INFO] Enable KPROBES
+        scripts/config --file "arch/$ARCH/configs/$KERNEL_DEFCONFIG" -e MODULES -e KPROBES -e HAVE_KPROBES -e KPROBE_EVENTS     # Enable KPROBES
+    else
+        echo [INFO] KernelSU patch
+        scripts/config --file "arch/$ARCH/configs/$KERNEL_DEFCONFIG" -e KSU     # Enable KSU
+        git apply $BUILDER_PATH/ksu-patch/*.patch       # Apply kenelSU patchs
+    fi
 }
 
-unsetup_kernelsu() {
+setup_void_fix(){
     cd $KERNEL_PATH
-    test -e "$KERNEL_PATH/drivers/kernelsu" && rm "$KERNEL_PATH/drivers/kernelsu"
-    grep -q "kernelsu" "$KERNEL_PATH/drivers/Makefile" && sed -i '/kernelsu/d' "$KERNEL_PATH/drivers/Makefile"
-    grep -q "kernelsu" "$KERNEL_PATH/drivers/Kconfig" && sed -i '/kernelsu/d' "$KERNEL_PATH/drivers/Kconfig"
+    git apply $BUILDER_PATH/fix-void-err-patch/*.patch
+}
+
+clean(){
+    # Clean garbage
+    echo [INFO] Clean garbage 
+    cd $KERNEL_PATH
+    make mrproper
+    # git reset --hard HEAD     # edited code will be cleaned up
+    # test -d $KERNEL_PATH/out/arch/arm64/boot && rm -rf $KERNEL_PATH/out/arch/arm64/boot/*
 }
 
 build_kernel() {
     cd $KERNEL_PATH
-    make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld $KERNEL_DEFCONFIG
-    # Disable LTO
-    if [[ $(echo "$(awk '/MemTotal/ {print $2}' /proc/meminfo) < 16000000" | bc -l) -eq 1 ]]; then
-        scripts/config --file out/.config -d LTO -d LTO_CLANG -d THINLTO -e LTO_NONE
-    fi
-    # Delete old files
-    test -d $KERNEL_PATH/out/arch/arm64/boot && rm -rf $KERNEL_PATH/out/arch/arm64/boot/*
+  # Disable LTO
+    # if [[ $(echo "$(awk '/MemTotal/ {print $2}' /proc/meminfo) < 16000000" | bc -l) -eq 1 ]]; then
+    #     scripts/config --file out/.config -d LTO -d LTO_CLANG -d THINLTO -e LTO_NONE
+    # fi
+
     # Begin compile
-    time make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 -j`nproc` CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee kernel.log
+    make  O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld $KERNEL_DEFCONFIG
+    time make -j$(nproc --all) O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64  CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee kernel.log
 }
 
 make_anykernel3_zip() {
     cd $KERNEL_PATH
-    test -d $KERNEL_PATH/AnyKernel3 || rm -rf $KERNEL_PATH/AnyKernel3
-    git clone https://gitlab.com/inferno0230/AnyKernel3 --depth=1 $KERNEL_PATH/AnyKernel3
-    if test -e $KERNEL_PATH/out/arch/arm64/boot/Image && test -d $KERNEL_PATH/AnyKernel3; then
-       zip_name="ONEPLUS9RT-v5.4.$(grep "^SUBLEVEL =" Makefile | awk '{print $3}')-$(date +"%Y%m%d").zip"
-       cd $KERNEL_PATH/AnyKernel3
-       cp $KERNEL_PATH/out/arch/arm64/boot/Image $KERNEL_PATH/AnyKernel3
-       zip -r ${zip_name} *
-       mv ${zip_name} $KERNEL_PATH/out/arch/arm64/boot
-       cd $KERNEL_PATH
+        if test -e $KERNEL_PATH/out/arch/arm64/boot/Image
+        then
+            zip_name="${KERNEL_DEFCONFIG%_*}-$(grep "^VERSION =" Makefile | awk '{print $3}').$(grep "^PATCHLEVEL =" Makefile | awk '{print $3}').$(grep "^SUBLEVEL =" Makefile | awk '{print $3}')-$(git rev-parse --short HEAD)-$(date +"%Y%m%d").zip"
+            cp $KERNEL_PATH/out/arch/arm64/boot/Image $ANYKERNEL_PATH
+            cd $ANYKERNEL_PATH
+            zip -r ${zip_name} ./*
+            mv ${zip_name} $KERNEL_PATH/out/
+            echo [INFO] out $zip_name to $KERNEL_PATH/out
+        else
+        echo [INFO] Build failed
     fi
 }
 
@@ -108,23 +108,28 @@ setup_export
 
 # update_kernel   //Please uncomment if you need it
 
-if test -e $CLANG_PATH/env_is_setup; then
-   echo [INFO]Environment have been setup!
-else
-   setup_environment
-   touch $CLANG_PATH/env_is_setup
+if test $SETUP_ENVIRONMENT = true 
+then
+    setup_environment
 fi
 
-if test "$SETUP_KERNELSU" == "true"; then
+if test $SETUP_KERNELSU = true
+then
    setup_kernelsu
+   echo [INFO] KerneSU Set up
 else
    echo [INFO] KernelSU will not be Compiled
-   unsetup_kernelsu
 fi
 
+if test $VOID_FIX_PATCH = true
+then    
+    echo [INFO]Applay fix-void-err-patch
+    setup_void_fix
+fi
+
+clean
 build_kernel
 
 make_anykernel3_zip
-cd $KERNEL_PATH
-echo [INFO] Products are put in $KERNEL_PATH/out/arch/arm64/boot
-echo [INFO] Done.
+
+echo [INFO] Done
